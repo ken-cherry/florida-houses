5592fe94aabe9aad98348e29cd88d293a39d30f6
ken-cherry <ken@cherrywebdev.com>
2024-03-06 18:49:04 -0600

    Removed: duplicate data.

--- src/data.js
+++ src/data.js
@@ -10,31 +10,9 @@ export const orlando = [
     directions:
       "https://www.google.com/maps/dir//2617+Dobbin+Dr,+Orlando,+FL+32817/@28.5831504,-81.2505711,17z/data=!4m9!4m8!1m0!1m5!1m1!1s0x88e768aca1e8650f:0x2e586b6632290075!2m2!1d-81.2505711!2d28.5831504!3e0?entry=ttu",
   },
-  {
-    image:
-      "https://photos.zillowstatic.com/fp/d8a0c1d5ffb7ce73af1bdf7b3ae695a9-cc_ft_768.webp",
-    alt: "may not be available anymore",
-    address: "2617 Dobbin Dr",
-    city: "Orlando",
-    link: "https://www.zillow.com/homedetails/2617-Dobbin-Dr-Orlando-FL-32817/46074613_zpid/",
-    price: "1,750",
-    directions:
-      "https://www.google.com/maps/dir//2617+Dobbin+Dr,+Orlando,+FL+32817/@28.5831504,-81.2505711,17z/data=!4m9!4m8!1m0!1m5!1m1!1s0x88e768aca1e8650f:0x2e586b6632290075!2m2!1d-81.2505711!2d28.5831504!3e0?entry=ttu",
-  },
 ];
 
 export const poinciana = [
-  {
-    image:
-      "https://www.compass.com/m/4315348441c3ec357ff15d4149e73c292c6366a2_img_0_82af1/640x480.webp",
-    alt: "may not be available anymore",
-    address: "620 Robin Ln",
-    city: "Poinciana",
-    link: "https://www.compass.com/listing/620-robin-lane-poinciana-fl-34759/1517490033143822465/",
-    price: "1,665",
-    directions:
-      "https://www.google.com/maps/place/620+Robin+Ln,+Kissimmee,+FL+34759/@28.0998018,-81.462723,17z/data=!3m1!4b1!4m6!3m5!1s0x88dd9eed9ac76a61:0x602129cfa0756be9!8m2!3d28.0997971!4d-81.4601481!16s%2Fg%2F11c1zw_mp5?entry=ttu",
-  },
   {
     image:
       "https://www.compass.com/m/4315348441c3ec357ff15d4149e73c292c6366a2_img_0_82af1/640x480.webp",
