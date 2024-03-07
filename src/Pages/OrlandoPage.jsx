bcf3ad6fc8e274ac4e000c0899d62cac50694ee4
ken-cherry <ken@cherrywebdev.com>
2024-03-06 19:28:37 -0600

    Added: button to Orlando page

--- src/Pages/OrlandoPage.jsx
+++ src/Pages/OrlandoPage.jsx
@@ -23,7 +23,7 @@ const OrlandoPage = () => {
                 <h2>${price}</h2>
                 <h3>
                   <a href={link} target="_blank" rel="noreferrer">
-                    Listing
+                    <button className="btn">Listing</button>
                   </a>
                 </h3>
               </div>
