150a8b21cdd80206ab334c2f0361a5ab11abb10e
ken-cherry <ken@cherrywebdev.com>
2024-03-14 11:19:22 -0500

    Edited: index.css .page

--- src/Pages/PoincianaPage.jsx
+++ src/Pages/PoincianaPage.jsx
@@ -12,24 +12,22 @@ const PoincianaPage = () => {
           {poinciana.map((item) => {
             const { image, alt, address, city, link, price, directions } = item;
             return (
-              <>
-                <div className="house-listing" key={nanoid()}>
-                  <h2>{city}</h2>
-                  <div className="image-house">
-                    <a href={directions} target="_blank" rel="noreferrer">
-                      <img src={image} alt={alt} className="image" />
-                    </a>
-                  </div>
-                  <h2>{address}</h2>
-                  <h2>${price}</h2>
-                  <h3 className="buttons">
-                    <a href={link} target="_blank" rel="noreferrer">
-                      <button className="btn">Listing</button>
-                    </a>
-                    <button className="btn remove-btn">Remove</button>
-                  </h3>
+              <div className="house-listing" key={nanoid()}>
+                <h2>{city}</h2>
+                <div className="image-house">
+                  <a href={directions} target="_blank" rel="noreferrer">
+                    <img src={image} alt={alt} className="image" />
+                  </a>
                 </div>
-              </>
+                <h2>{address}</h2>
+                <h2>${price}</h2>
+                <h3 className="buttons">
+                  <a href={link} target="_blank" rel="noreferrer">
+                    <button className="btn">Listing</button>
+                  </a>
+                  <button className="btn remove-btn">Remove</button>
+                </h3>
+              </div>
             );
           })}
         </div>
